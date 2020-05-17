6cc20f46372854e400291a54d329162cc195053b
DimaStill <dimastill123@gmail.com>
2020-05-17 23:02:38 +0300

    Add DbSet Genre and Studio to DB Context

--- AnimeAPI/AnimeAPI/AnimeContext.cs
+++ AnimeAPI/AnimeAPI/AnimeContext.cs
@@ -13,10 +13,13 @@ namespace AnimeAPI
         public DbSet<User> Users { get; set; }
         public DbSet<Anime> Animes { get; set; }
         public DbSet<Manga> Mangas { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Studio> Studios { get; set; }
 
         public AnimeContext(DbContextOptions<AnimeContext> options)
             : base(options)
         {
+            /*Database.EnsureDeleted();*/
             Database.EnsureCreated();
         }
     }
