59b19f5e482f3d91e854fb083c1021878c3c40e2
DimaStill <dimastill123@gmail.com>
2020-05-25 22:57:48 +0300

    Create Manga List component

--- AnimeMobileApp/src/app/app-routing.module.ts
+++ AnimeMobileApp/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { UserIdentificationComponent } from "./user-identification/user-identification.component";
 import { AnimeListComponent } from "./anime-list/anime-list.component";
+import { MangaListComponent } from "./manga-list/manga-list.component";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/anime-list", pathMatch: "full" },
+    { path: "", redirectTo: "/manga-list", pathMatch: "full" },
     { path: "identification-user", component: UserIdentificationComponent },
     { path: "anime-list", component: AnimeListComponent },
+    { path: "manga-list", component: MangaListComponent },
     { path: "browse", loadChildren: () => import("~/app/browse/browse.module").then((m) => m.BrowseModule) },
     { path: "search", loadChildren: () => import("~/app/search/search.module").then((m) => m.SearchModule) },
     { path: "featured", loadChildren: () => import("~/app/featured/featured.module").then((m) => m.FeaturedModule) },
--- AnimeMobileApp/src/app/manga-list/manga-list.component.ts
+++ AnimeMobileApp/src/app/manga-list/manga-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
+import * as app from "tns-core-modules/application";
 
 @Component({
 	moduleId: module.id,
 	selector: 'manga-list',
 	templateUrl: './manga-list.component.html',
-	styleUrls: ['./manga-list.component.css']
+	styleUrls: ['./manga-list.component.scss']
 })
 
 export class MangaListComponent implements OnInit {
@@ -12,4 +14,9 @@ export class MangaListComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit() { }
+	
+    onDrawerButtonTap(): void {
+        const sideDrawer = <RadSideDrawer>app.getRootView();
+        sideDrawer.showDrawer();
+    }
 }
