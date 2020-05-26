9ace8d064bc6cdad48ca9463eca302b2329634a8
DimaStill <dimastill123@gmail.com>
2020-05-26 21:36:56 +0300

    Create anime info component

--- AnimeMobileApp/src/app/app-routing.module.ts
+++ AnimeMobileApp/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { UserIdentificationComponent } from "./user-identification/user-identification.component";
 import { AnimeListComponent } from "./anime-list/anime-list.component";
 import { MangaListComponent } from "./manga-list/manga-list.component";
+import { AnimeInfoComponent } from "./anime-list/anime-info/anime-info.component";
+import { MangaInfoComponent } from "./manga-list/manga-info/manga-info.component";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/manga-list", pathMatch: "full" },
+    { path: "", redirectTo: "/anime-info", pathMatch: "full" },
     { path: "identification-user", component: UserIdentificationComponent },
     { path: "anime-list", component: AnimeListComponent },
     { path: "manga-list", component: MangaListComponent },
+    { path: "anime-info", component: AnimeInfoComponent },
+    { path: "manga-info", component: MangaInfoComponent },
     { path: "browse", loadChildren: () => import("~/app/browse/browse.module").then((m) => m.BrowseModule) },
     { path: "search", loadChildren: () => import("~/app/search/search.module").then((m) => m.SearchModule) },
     { path: "featured", loadChildren: () => import("~/app/featured/featured.module").then((m) => m.FeaturedModule) },
