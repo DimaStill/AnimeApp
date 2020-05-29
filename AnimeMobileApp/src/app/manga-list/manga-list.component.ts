3dc766d7f57f0bc989f3bd842675e23c01ccb757
DimaStill <dimastill123@gmail.com>
2020-05-29 22:45:27 +0300

    Create Manga list component

--- AnimeMobileApp/src/app/manga-list/manga-list.component.ts
+++ AnimeMobileApp/src/app/manga-list/manga-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
 import * as app from "tns-core-modules/application";
+import { Manga } from '../models/manga';
+import { RouterExtensions } from 'nativescript-angular/router';
+import { MangaService } from '../services/mangaService';
 
 @Component({
 	moduleId: module.id,
@@ -10,13 +13,40 @@ import * as app from "tns-core-modules/application";
 })
 
 export class MangaListComponent implements OnInit {
+	mangas: Array<Manga>;
 
-	constructor() { }
+	constructor(private routerExtensions: RouterExtensions,
+		private mangaService: MangaService) { }
 
-	ngOnInit() { }
+	ngOnInit() { 
+		this.getAllMangas();
+	}
 	
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
-    }
+	}
+
+	getAllMangas() {
+		this.mangaService.getAllMangas().subscribe(manga => {
+			this.mangas = manga;
+		});
+	}
+	
+	getCountRows() {
+		return 'auto'.repeat(Math.ceil(this.mangas.length / 2));
+	}
+
+	getRow(index: number) {
+		return Math.floor(index / 2);
+	}
+
+	getCol(index: number) {
+		return index % 2;
+	}
+	
+	selectAnime(manga: Manga) {
+		this.mangaService.setActiveManga(manga);
+		this.routerExtensions.navigate(['/manga-info'], { clearHistory: true });
+	}
 }
