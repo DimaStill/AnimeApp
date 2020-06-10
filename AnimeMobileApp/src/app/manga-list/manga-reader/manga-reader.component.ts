f735730c9449de425d87821b7fae391413fcaf58
DimaStill <dimastill123@gmail.com>
2020-06-10 22:15:04 +0300

    Fix display manga pages

--- AnimeMobileApp/src/app/manga-list/manga-reader/manga-reader.component.ts
+++ AnimeMobileApp/src/app/manga-list/manga-reader/manga-reader.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 import { MangaService } from '~/app/services/mangaService';
 import { Anime } from '~/app/models/anime';
 import { Manga } from '~/app/models/manga';
 import { MangaPages } from '~/app/models/mangaPages';
+import {ImageSource, fromFile, fromResource, fromBase64} from "tns-core-modules/image-source";
+import { Image } from "tns-core-modules/ui/image";
 
 @Component({
 	moduleId: module.id,
@@ -15,7 +17,11 @@ export class MangaReaderComponent implements OnInit {
 	manga: Manga;
 	mangaPages: MangaPages;
 	currentPage = 0;
-
+	page = '';
+	imageSource;
+	@ViewChild("photoImage", {static: false}) photoImage: ElementRef;
+	photo: Image;
+	
 	constructor(private mangaService: MangaService) { }
 
 	ngOnInit() {
@@ -26,15 +32,39 @@ export class MangaReaderComponent implements OnInit {
 	getMangaPages() {
 		this.mangaService.getMangaPages(this.manga.id).subscribe(pages => {
 			this.mangaPages = pages;
+			this.imageSource = new ImageSource();
+			const loadedBase64 = this.imageSource.loadFromBase64(this.mangaPages.pages[0].pageImageBase64);
+			console.log(loadedBase64);
+			if (loadedBase64) {
+			  let photo = <Image>this.photoImage.nativeElement;
+			  photo.imageSource = this.imageSource;
+			  this.photo = photo;
+			}
 		});
 	}
 
 	goNextPage() {
 		this.currentPage++;
+		this.imageSource = new ImageSource();
+		const loadedBase64 = this.imageSource.loadFromBase64(this.mangaPages.pages[this.currentPage].pageImageBase64);
+		console.log(loadedBase64);
+		if (loadedBase64) {
+		  let photo = <Image>this.photoImage.nativeElement;
+		  photo.imageSource = this.imageSource;
+		  this.photo = photo;
+		}
 	}
 
 	goPrevPage() {
 		this.currentPage--;
+		this.imageSource = new ImageSource();
+		const loadedBase64 = this.imageSource.loadFromBase64(this.mangaPages.pages[this.currentPage].pageImageBase64);
+		console.log(loadedBase64);
+		if (loadedBase64) {
+		  let photo = <Image>this.photoImage.nativeElement;
+		  photo.imageSource = this.imageSource;
+		  this.photo = photo;
+		}
 	}
 	
 	getActiveManga() {
@@ -45,10 +75,10 @@ export class MangaReaderComponent implements OnInit {
 	}
 
 	getPage(index: number) {
+		
 		if (this.mangaPages) {
 			console.log(this.mangaPages.pages[this.currentPage].pageImageBase64);
 			return 'data:image/png;base64,' + this.mangaPages.pages[this.currentPage].pageImageBase64;
 		}
-		return '';
 	}
 }
